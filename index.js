9b9481aff903cfe6b70f14ececac799a5355bf85
herloncosta <herlon36@gmail.com>
2024-11-16 14:50:00 -0300

    configurin linter

--- index.js
+++ index.js
@@ -1,61 +1,61 @@
-import axios from "axios";
-import cors from "cors";
-import express from "express";
+import axios from 'axios'
+import cors from 'cors'
+import express from 'express'
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 app.use(
-	cors({
-		origin: "*",
-		methods: ["POST"],
-		credentials: true,
-	}),
-);
+    cors({
+        origin: '*',
+        methods: ['POST'],
+        credentials: true,
+    }),
+)
 
-app.post("/ai", async (req, res) => {
-	const { prompt } = req.body;
-	try {
-		res.writeHead(200, {
-			"Content-Type": "text/event-stream",
-			"Cache-Control": "no-cache",
-			Connection: "keep-alive",
-		});
+app.post('/ai', async (req, res) => {
+    const { prompt } = req.body
+    try {
+        res.writeHead(200, {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            Connection: 'keep-alive',
+        })
 
-		const response = await axios.post(
-			"http://localhost:11434/api/generate",
-			{
-				model: "llama3",
-				prompt,
-				stream: true,
-			},
-			{
-				responseType: "stream",
-			},
-		);
+        const response = await axios.post(
+            'http://localhost:11434/api/generate',
+            {
+                model: 'llama3',
+                prompt,
+                stream: true,
+            },
+            {
+                responseType: 'stream',
+            },
+        )
 
-		response.data.on("data", (data) => {
-			try {
-				const message = JSON.parse(data.toString());
-				console.log(message);
-				res.write(JSON.stringify(message));
-			} catch (error) {
-				console.log(`Error on parsing data: ${error}`);
-			}
-		});
+        response.data.on('data', (data) => {
+            try {
+                const message = JSON.parse(data.toString())
+                console.log(message)
+                res.write(JSON.stringify(message))
+            } catch (error) {
+                console.log(`Error on parsing data: ${error}`)
+            }
+        })
 
-		response.data.on("end", () => {
-			res.end();
-		});
+        response.data.on('end', () => {
+            res.end()
+        })
 
-		response.data.on("error", (err) => {
-			console.log(`Error on stream: ${err}`);
-			res.status(500).send("Error on processing response...");
-		});
-	} catch (error) {
-		res.status(404).json({ error: error.message });
-	}
-});
+        response.data.on('error', (err) => {
+            console.log(`Error on stream: ${err}`)
+            res.status(500).send('Error on processing response...')
+        })
+    } catch (error) {
+        res.status(404).json({ error: error.message })
+    }
+})
 
 app.listen(3000, () => {
-	console.log("Server is running on port 3000");
-});
+    console.log('Server is running on port 3000')
+})
