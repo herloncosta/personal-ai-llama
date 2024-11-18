a1d4ba6379e2e80ae1ef08ecf8e0f044c62767fc
herloncosta <herlon36@gmail.com>
2024-11-18 12:56:03 -0300

    adding reset css

--- frontend/app.js
+++ frontend/app.js
@@ -1,59 +1,70 @@
-const app = document.getElementById('app')
-const promptInputEl = document.getElementById('prompt')
-const sendPromptButtonEl = document.querySelector('button')
-const API_URL = 'http://localhost:3001/ai'
+const app = document.getElementById("app");
+const promptInputEl = document.getElementById("prompt");
+const sendPromptButtonEl = document.querySelector("button");
+const API_URL = "http://localhost:3001/ai";
 
 async function sendPrompt(prompt) {
-    const payload = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ prompt }),
-    }
-    const response = await fetch(API_URL, payload)
-    return response
+	const payload = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ prompt }),
+	};
+	try {
+		const response = await fetch(API_URL, payload);
+		return { response, statusCode: response.status };
+	} catch (error) {
+		app.innerText = "Servidor indisponível!";
+		return { error: error.message };
+	}
 }
 
 async function request(prompt) {
-    const response = await sendPrompt(prompt)
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`)
-    }
+	if (!prompt || prompt.trim() === "") {
+		app.innerText = "Digite uma pergunta!";
+		return;
+	}
+	const { response, error, statusCode } = await sendPrompt(prompt);
+	if (error) {
+		app.innerText =
+			"Servidor indisponível! Tente novamente, ou contate o administrador.";
+		console.warn(error);
+		return;
+	}
+	const reader = response.body.getReader();
+	const decoder = new TextDecoder();
+	let buffer = "";
 
-    const reader = response.body.getReader()
-    const decoder = new TextDecoder()
-    let buffer = ''
-
-    while (true) {
-        const { done, value } = await reader.read()
-        if (done) {
-            break
-        }
-        buffer += decoder.decode(value)
-        const parseResponseToJson = parseStreamData(buffer)
-        let result = ''
-        for (const text of parseResponseToJson) {
-            result += text.response
-        }
-        app.innerText = result
-    }
+	while (true) {
+		const { done, value } = await reader.read();
+		if (done) {
+			break;
+		}
+		buffer += decoder.decode(value);
+		const parseResponseToJson = parseStreamData(buffer);
+		let result = "";
+		for (const text of parseResponseToJson) {
+			result += text.response;
+		}
+		app.innerText = result;
+	}
 }
 
 function parseStreamData(data) {
-    const jsonParts = data.match(/{[^{}]*}/g)
-    if (!jsonParts) {
-        console.error('Não foi possível encontrar objetos JSON na string')
-        return
-    }
-    const parsedObjects = jsonParts.map(part => JSON.parse(part))
-    return parsedObjects
+	const jsonParts = data.match(/{[^{}]*}/g);
+	if (!jsonParts) {
+		console.error("Não foi possível encontrar objetos JSON na string");
+		return;
+	}
+	const parsedObjects = jsonParts.map((part) => JSON.parse(part));
+	return parsedObjects;
 }
 
-sendPromptButtonEl.addEventListener('click', event => {
-    event.preventDefault()
-    app.innerText = 'Loading...'
-    const prompt = promptInputEl.value
-    request(prompt)
-    promptInputEl.value = ''
-})
+sendPromptButtonEl.addEventListener("click", (event) => {
+	event.preventDefault();
+	app.innerText = "Loading...";
+	const prompt = promptInputEl.value;
+	request(prompt);
+	promptInputEl.value = "";
+});
